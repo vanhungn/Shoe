6bc13856219be5294761e560c73a55e8ef18049a
vanhungn <vanhungnvh1712004@gmail.com>
2025-09-21 15:40:22 +0700

    sdfd

--- app.js
+++ app.js
@@ -28,13 +28,12 @@ app.use('/users', usersRouter);
 app.use(function (req, res, next) {
   next(createError(404));
 });
-app.use(cors());
+app.use('*',cors());
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
