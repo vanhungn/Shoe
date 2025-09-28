af9fb4ffe6d9271744210ed3b41db0d1419faed1
vanhungn <vanhungnvh1712004@gmail.com>
2025-09-28 13:50:54 +0700

    sd

--- helps/email.js
+++ helps/email.js
@@ -1,7 +1,9 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config()
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+     host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.USER_EMAIL,
         pass: process.env.PASSWORD_EMAIL,
