3151605325c2b2af75b9f8881cee32f0530ffae9
vanhungn <vanhungnvh1712004@gmail.com>
2025-10-09 17:57:37 +0700

    ok

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,33 +1,34 @@
 const jwt = require("jsonwebtoken")
 require('dotenv').config();
-const Auth=(req,res,next)=>{
+const Auth = (req, res, next) => {
     try {
-    const header = req.headers['authorization'];
-        if(!header){
+        const header = req.headers['authorization'];
+        if (!header) {
             return res.status(400).json({
-                message:"Token is incorrect"
+                message: "Token is incorrect"
             })
         }
-        jwt.verify(header,process.env.REFRESH_KEY,(err,user)=>{
-            if(err){
-                if(err.name = 'TokenExpiredError'){
-                     return res.status(404).json({
+        jwt.verify(header, process.env.SECRET_KEY, (err, user) => {
+            if (err) {
+                console.log(err)
+                if (err.name === 'TokenExpiredError') {
+                    return res.status(404).json({
                         message: 'Token expired',
                     });
-                }else {
+                } else {
                     res.status(400).json({ error: 'Invalid token' });
                     return;
                 }
-            }else {
+            } else {
                 req.user = user;
                 next();
             }
         })
     } catch (error) {
-         return res.status(404).json({
+        return res.status(404).json({
             message: error,
         });
     }
-    
+
 }
 module.exports = Auth
